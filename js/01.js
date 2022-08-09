56528dc80644cb82696ea611930f79e07bb80bb6
gabrielg9509 <64519565+gabrielg9509@users.noreply.github.com>
2022-08-08 23:18:06 -0500

    Add files via upload

---
+++ js/01.js
@@ -0,0 +1,23 @@
+
+/*html*/
+function CambiaTexto(){
+    document.getElementById("test").innerHTML="que onda shavo";
+}
+
+
+function NumeroRandom(){
+    document.write(Math.random()*10);
+}
+
+
+/*css*/
+
+function CambiarColorRojo(){
+    document.getElementById("texto_prueba_colores").style.color="red";
+}
+
+function CambiarColorAzul(){
+    document.getElementById("texto_prueba_colores").style.color="blue";
+}
+
+
